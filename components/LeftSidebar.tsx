849e6ef2668e7ad5cc938045e0710ae0a4a4871f
SAURABH SAHGAL <saurabhsahgal2023@gmail.com>
2025-08-13 14:24:33 +0530

    Update LeftSidebar.tsx

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -21,7 +21,7 @@ const LeftSidebar: React.FC<LeftSidebarProps> = ({ user }) => {
                     {user.intro.website && (
                         <div className="flex items-center space-x-2">
                             <svg className="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" /></svg>
-                            <a href={`https://${user.intro.linkedin}`} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">{user.intro.linkedin}</a>
+                            <a href={`https://${user.intro.website}`} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">{user.intro.website}</a>
                         </div>
                     )}
                     <div className="flex items-center space-x-2">
